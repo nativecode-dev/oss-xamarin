6cbc94f18c1095958c9bde85f129e67544d0f25b
Mike Pham <mpham@nativecode.com>
2015-07-10 16:08:03 -0500

    Updating to Xamarin.Forms 1.4.3.6376.

--- src/Demo/Demo/ViewModels/ViewModel.cs
+++ src/Demo/Demo/ViewModels/ViewModel.cs
@@ -1,10 +1,15 @@
 ﻿namespace Demo.ViewModels
 {
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
     using PropertyChanged;
 
     [ImplementPropertyChanged]
-    public abstract class ViewModel
+    public abstract class ViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         /// <summary>
         /// Gets or sets a value indicating whether this instance is busy.
         /// </summary>
@@ -14,5 +19,15 @@
         /// Gets or sets the title.
         /// </summary>
         public string Title { get; set; }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            var handler = this.PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(property));
+            }
+        }
     }
 }
